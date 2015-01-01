dc08c5c8c7967248fd65cbd3839d10d89b625f10
Mageswaran.D <mageswaran1989@gmail.com>
2015-01-01 21:24:34 +0530

    latex doc fo hopfield added

--- include/aja/ann/network.hpp
+++ include/aja/ann/network.hpp
@@ -3,21 +3,5 @@
 #ifndef AJA_NETWORK_H
 #define AJA_NETWORK_H
 
-#include <stdio.h>
-#include <iostream>
-//TODO: Replace with vexCL
-#include <math.h>
-#include <aja/ann/neuron.hpp>
-
-class network
-{
-public:
-    neuron
-    nrn[4];
-    int output[4];
-    int threshld(int) ;
-    void activation(int j[4]);
-    network(int*,int*,int*,int*);
-};
 
 #endif
