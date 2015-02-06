1a6a792f1b49af035778759a502273dd0c2822c8
Mageswaran.D <mageswaran1989@gmail.com>
2015-02-06 22:53:32 +0530

    updating backpropagation class

---
+++ include/aja/ann/perceptron.hpp
@@ -0,0 +1,14 @@
+/// Class that represents a single neuron
+/// A single neuron shall have
+/// * weight
+/// * input
+/// * activation function
+/// * thersold value
+/// * output
+
+#ifndef AJA_NEURON_H
+#define AJA_NEURON_H
+
+
+
+#endif
