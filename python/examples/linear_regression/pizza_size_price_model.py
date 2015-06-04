4e8524a7ca75fcd8c87f5415acdd5ce263059b9c
Mageswaran.D <mageswaran1989@gmail.com>
2015-06-04 17:57:45 +0530

    ml_with_spark: adding collabrative filter example

--- python/examples/linear_regression/pizza_size_price_model.py
+++ python/examples/linear_regression/pizza_size_price_model.py
@@ -1,7 +1,20 @@
+import matplotlib.pyplot as plt
+X = [[6], [8], [10], [14], [18]] # Size of Pizza
+y = [[7], [9], [13], [17.5], [18]]  # Price of Pizza
+plt.figure()
+plt.title('Pizza price plotted against diameter')
+plt.xlabel('Diameter in inches')
+plt.ylabel('Price in dollars')
+plt.plot(X, y, 'k.')
+plt.axis([0, 25, 0, 25])
+plt.grid(True)
+plt.show()
+
+
 from sklearn.linear_model import LinearRegression
 # Training data
-X = [[6], [8], [10], [14], [18]]
-y = [[7], [9], [13], [17.5], [18]]
+X = [[6], [8], [10], [14], [18]] # Size of Pizza 
+y = [[7], [9], [13], [17.5], [18]] # Price of Pizza
 # Create and fit the model
 model = LinearRegression()
 model.fit(X, y)
