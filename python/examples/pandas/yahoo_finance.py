a6a4f3e81aa28cebdd699673fac9473548cf8f64
Mageswaran.D <mageswaran1989@gmail.com>
2015-06-15 09:57:25 +0530

    added python pandas example

---
+++ python/examples/pandas/yahoo_finance.py
@@ -0,0 +1,19 @@
+import pandas.io.data as web
+all_data = {}
+for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']:
+  all_data[ticker] = web.get_data_yahoo(ticker, '1/1/2000', '1/1/2010')
+
+price = DataFrame({tic: data['Adj Close']
+                  for tic, data in all_data.iteritems()})
+
+volume = DataFrame({tic: data['Volume']
+                   for tic, data in all_data.iteritems()})
+
+returns = price.pct_change()
+
+returns.tail()
+
+
+
+
+
