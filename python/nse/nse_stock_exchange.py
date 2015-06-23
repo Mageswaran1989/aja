f469e7fd025b0aa971a94ad44f6ec177e7a98c21
Mageswaran Dhandapani <mageswaran.dhanadapani@harman.com>
2015-06-23 19:53:42 +0530

    nese script updated

--- python/nse/nse_stock_exchange.py
+++ python/nse/nse_stock_exchange.py
@@ -1,6 +1,11 @@
 from nsetools import Nse
+from pandas import Series, DataFrame
+import pandas as pd
+import time
+import datetime
 
 # Utility Functions
+
 def dict_compare(d1, d2):
     d1_keys = set(d1.keys()) #set with unique keys
     d2_keys = set(d2.keys())
@@ -11,11 +16,27 @@ def dict_compare(d1, d2):
     items_modified = {key : (d1[key], d2[key]) for key in intersect_keys if d1[key] != d2[key]}
     items_same = set(key for key in intersect_keys if d1[key] == d2[key])
     return items_added, items_removed, items_modified, items_same
+#added, removed, modified, same = dict_compare(x, y)
 
 
-added, removed, modified, same = dict_compare(x, y)
+def dump_stock_codes_to_disk(nse):
+    stock_codes = nse.get_stock_codes()
+    stock_codes_dataframe = DataFrame(stock_codes.items(), columns = ['NSE ID', 'Full Name'])
+    stock_codes_dataframe.to_csv('stock_codes.csv')
 
+
+#################################################################################################
+#Intialize Nse
 nse = Nse()
 
-stock_name = nse.get_quote('infy')
+stock_name = raw_input("Enter the stock nse id (eg: infy):")
+stock = nse.get_quote(stock_name)
+
+ts = time.time()
+timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+transposedEntry = DataFrame(stock.values(), index=stock.keys(), columns=[timeStamp])
+entry = transposedEntry.T
+entry.to_csv(stock_name+".csv")
+
 
