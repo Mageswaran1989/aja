df187ba207d462b78e6644d5660f4decfe5dbaa0
Mageswaran.D <mageswaran1989@gmail.com>
2015-07-26 18:37:07 +0530

    some cleanup

--- scala/src/scala/org/aja/se/Nse.scala
+++
@@ -1,7 +0,0 @@
-package org.aja.se
-
-case class Nse extends SEBase {
-
-  @override def getQuoteUrl: String = "http://nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?"
-  @override def
-}
