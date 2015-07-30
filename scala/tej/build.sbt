9edb54781dfbe018f6c1824f9639702540102458
Mageswaran.D <mageswaran1989@gmail.com>
2015-07-30 10:18:26 +0530

    spark streaming example added

--- scala/tej/build.sbt
+++ scala/tej/build.sbt
@@ -6,6 +6,12 @@ scalaVersion := "2.11.7"
 
 ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
 
+libraryDependencies += "org.twitter4j" % "twitter4j-core" % "4.0.4"
+
+libraryDependencies += "org.twitter4j" % "twitter4j-stream" % "4.0.4"
+libraryDependencies += "com.google.code.gson" % "gson" % "2.3"
+libraryDependencies += "commons-cli" % "commons-cli" % "1.2"
+
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.4.1",
   "org.apache.spark" %% "spark-mllib" % "1.4.1",
@@ -17,8 +23,19 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-streaming-twitter" % "1.4.1"
 )
 
-libraryDependencies += "org.twitter4j" %% "twitter4j-core" % "3.0.3"
 
-libraryDependencies += "org.twitter4j" %% "twitter4j-stream" % "3.0.3"
 
-resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
+resolvers ++= Seq(
+//  "Twitter4J Repository" at "http://twitter4j.org/maven2/",
+//  "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/",
+//  "Spray Repository" at "http://repo.spray.cc/",
+//  " loudera Repository" at "https://repository.cloudera.com/artifactory/cloudera-repos/",
+//  "Akka Repository" at "http://repo.akka.io/releases/"
+//  "Apache HBase" at "https://repository.apache.org/content/repositories/releases",
+//  "Twitter Maven Repo" at "http://maven.twttr.com/",
+//  "scala-tools" at "https://oss.sonatype.org/content/groups/scala-tools",
+//  "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+//  "Second Typesafe repo" at "http://repo.typesafe.com/typesafe/maven-releases/",
+//  "Mesosphere Public Repository" at "http://downloads.mesosphere.io/maven",
+//  Resolver.sonatypeRepo("public")
+)
---
+++ scala/tej/src/test/scala/org/aja/tej/test/streaming/Predict.scala
@@ -0,0 +1,58 @@
+package org.aja.tej.test.streaming
+
+/**
+ * Created by mageswaran on 30/7/15.
+ */
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.clustering.KMeansModel
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+ * Pulls live tweets and filters them for tweets in the chosen cluster.
+ */
+
+//${YOUR_SPARK_HOME}/bin/spark-submit \
+//--class "com.databricks.apps.twitter_classifier.Predict" \
+//--master ${YOUR_SPARK_MASTER:-local[4]} \
+//target/scala-2.10/spark-twitter-lang-classifier-assembly-1.0.jar \
+//${YOUR_MODEL_DIR:-/tmp/tweets/model} \
+//${CLUSTER_TO_FILTER:-7} \
+//--consumerKey ${YOUR_TWITTER_CONSUMER_KEY} \
+//--consumerSecret ${YOUR_TWITTER_CONSUMER_SECRET} \
+//--accessToken ${YOUR_TWITTER_ACCESS_TOKEN}  \
+//--accessTokenSecret ${YOUR_TWITTER_ACCESS_SECRET}
+
+
+object Predict {
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Usage: " + this.getClass.getSimpleName + " <modelDirectory>  <clusterNumber>")
+      System.exit(1)
+    }
+
+    val Array(modelFile, Utils.IntParam(clusterNumber)) =
+      Utils.parseCommandLineWithTwitterCredentials(args)
+
+    println("Initializing Streaming Spark Context...")
+    val conf = new SparkConf().setAppName(this.getClass.getSimpleName)
+    val ssc = new StreamingContext(conf, Seconds(5))
+
+    println("Initializing Twitter stream...")
+    val tweets = TwitterUtils.createStream(ssc, Utils.getAuth)
+    val statuses = tweets.map(_.getText)
+
+    println("Initalizaing the the KMeans model...")
+    val model = new KMeansModel(ssc.sparkContext.objectFile[Vector](modelFile.toString).collect())
+
+    val filteredTweets = statuses
+      .filter(t => model.predict(Utils.featurize(t)) == clusterNumber)
+    filteredTweets.print()
+
+    // Start the streaming computation
+    println("Initialization complete.")
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
