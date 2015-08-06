18459d037b2ba9a28032360198ab883f32cdd697
Mageswaran Dhandapani <mageswaran.dhanadapani@harman.com>
2015-08-06 19:38:18 +0530

    naive bayes + spark + intellij

--- scala/tej/src/test/scala/org/aja/tej/test/usecases/stumbleupon/classification/StumbleuponUtils.scala
+++ scala/tej/src/test/scala/org/aja/tej/test/usecases/stumbleupon/classification/StumbleuponUtils.scala
@@ -8,6 +8,40 @@ import org.apache.spark.{SparkContext, SparkConf}
 /**
  * Created by mdhandapani on 5/8/15.
  */
+
+/**
+ * Description:
+*==============================================
+FieldName 	            Type	  Description
+*==============================================
+url	                    string	Url of the webpage to be classified
+urlid	                  integer	StumbleUpon's unique identifier for each url
+boilerplate	            json	  Boilerplate text
+alchemy_category	      string	Alchemy category (per the publicly available Alchemy API found at www.alchemyapi.com)
+alchemy_category_score	double	Alchemy category score (per the publicly available Alchemy API found at www.alchemyapi.com)
+avglinksize	            double	    Average number of words in each link
+commonLinkRatio_1	      double	# of links sharing at least 1 word with 1 other links / # of links
+commonLinkRatio_2	      double	# of links sharing at least 1 word with 2 other links / # of links
+commonLinkRatio_3	      double	# of links sharing at least 1 word with 3 other links / # of links
+commonLinkRatio_4	      double	# of links sharing at least 1 word with 4 other links / # of links
+compression_ratio	      double	Compression achieved on this page via gzip (measure of redundancy)
+embed_ratio	            double	Count of number of <embed>  usage
+frameBased	            integer (0 or 1)	A page is frame-based (1) if it has no body markup but have a frameset markup
+frameTagRatio	          double	Ratio of iframe markups over total number of markups
+hasDomainLink	          integer (0 or 1)	True (1) if it contains an <a>  with an url with domain
+html_ratio	            double	Ratio of tags vs text in the page
+image_ratio	            double	Ratio of <img> tags vs text in the page
+is_news	                integer (0 or 1)	True (1) if StumbleUpon's news classifier determines that this webpage is news
+lengthyLinkDomain	      integer (0 or 1)	True (1) if at least 3 <a> 's text contains more than 30 alphanumeric characters
+linkwordscore	          double	Percentage of words on the page that are in hyperlink's text
+news_front_page	        integer (0 or 1)	True (1) if StumbleUpon's news classifier determines that this webpage is front-page news
+non_markup_alphanum_characters	integer	Page's text's number of alphanumeric characters
+numberOfLinks	          integer	Number of <a>  markups
+numwords_in_url	        double	Number of words in url
+parametrizedLinkRatio	  double	A link is parametrized if it's url contains parameters  or has an attached onClick event
+spelling_errors_ratio	  double	Ratio of words not found in wiki (considered to be a spelling mistake)
+label	                  integer (0 or 1)	User-determined label. Either evergreen (1) or non-evergreen (0); available for train.tsv only
+ */
 object StumbleuponUtils {
 
   def getSparkContext = {
@@ -15,13 +49,13 @@ object StumbleuponUtils {
     new SparkContext(conf)
   }
 
-  def getRawData = {
+  val getRawData = {
     val rawData = getSparkContext.textFile("data/stumbleupon/train_noheader.tsv")
     rawData.map(line => line.split("\t"))
     //records.first()s
-  }
+  } . cache
 
-  def getLabeledPoint {
+  val getLabeledPoint = {
     val records = getRawData
     records.map {r =>
       val trimmed = r.map(_.replaceAll("\"", ""))
@@ -31,23 +65,17 @@ object StumbleuponUtils {
     } . cache
   }
 
-  //data.cache
-  //val numData = data.count
+  def getNumData = {
+   getRawData.count
+  }
 
-  def getLabeledDataForNb = {
-    val records = getRawData
+  val getLabeledDataForNb = {
     //For Naive Bayes with -1 => 0
-    records.map { r =>
+    getRawData.map { r =>
       val trimmed = r.map(_.replaceAll("\"", ""))
       val label = trimmed(r.size - 1).toInt
       val features = trimmed.slice(4, r.size - 1).map(d => if (d == "?") 0.0 else d.toDouble).map(d => if (d < 0) 0.0 else d)
       LabeledPoint(label, Vectors.dense(features))
     } . cache
   }
-
-  def main(args: Array[String]) {
-    val nbModel = NaiveBayes.train(getLabeledDataForNb)
-  }
-
-
 }
