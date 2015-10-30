5e5ba269555d4964d17a1990c54e9ca2c88dbf4d
Mageswaran Dhandapani <mageswaran.dhanadapani@harman.com>
2015-10-30 21:51:39 +0530

    RDD serialization example

--- src/examples/scala/org/aja/tej/examples/spark/serialization/AllAboutSerialization.scala
+++ src/examples/scala/org/aja/tej/examples/spark/serialization/AllAboutSerialization.scala
@@ -1,5 +1,8 @@
 package org.aja.tej.examples.spark.serialization
 
+import org.aja.tej.utils.TejUtils
+import org.apache.spark.SparkContext
+
 /**
  * Created by mageswaran on 30/10/15.
  */
@@ -23,6 +26,75 @@ occur. [This lets you catch any bugs even before deploying to production]
 Links: http://stackoverflow.com/questions/22592811/task-not-serializable-java-io-notserializableexception-when-calling-function-ou
 http://erikerlandson.github.io/blog/2015/03/31/hygienic-closures-for-scala-function-serialization/
  */
-object AllAboutSerialization {
 
+//You know I am going to take your application down :)
+class ClassRDDManupulation_Ver1 {
+  def listIncrement(sc : SparkContext) = {
+
+    val listRDD = sc.parallelize(List(1,2,3,4,5,6,7,8,9,10), 2)
+    def increment(x: Int) = x + 1
+    println("ClassRDDManupulation_Ver1")
+    val incrementedListRDD = listRDD.map(increment(_))
+    incrementedListRDD.foreach(println)
+  }
+}
+
+class ClassRDDManupulation_Ver2 extends java.io.Serializable { //Did someone packed me?
+def listIncrement(sc : SparkContext) = {
+
+  val listRDD = sc.parallelize(List(1,2,3,4,5,6,7,8,9,10), 2)
+  def increment(x: Int) = x + 1
+  println("ClassRDDManupulation_Ver2")
+  val incrementedListRDD = listRDD.map(increment(_))
+  incrementedListRDD.foreach(println)
+}
+}
+
+class ClassRDDManupulation_Ver3 {
+  def listIncrement(sc : SparkContext) = {
+
+    val listRDD = sc.parallelize(List(1,2,3,4,5,6,7,8,9,10), 2)
+    val increment = (x: Int) => x + 1 //Hey there... now I am a first class citizen!
+    println("ClassRDDManupulation_Ver3")
+    val incrementedListRDD = listRDD.map(increment(_))
+    incrementedListRDD.foreach(println)
+  }
+}
+
+//Oh god I cant be keep incarnating!
+object ClassRDDManupulation_Ver4 {
+  def listIncrement(sc : SparkContext) = {
+
+    val listRDD = sc.parallelize(List(1,2,3,4,5,6,7,8,9,10), 2)
+    def increment(x: Int) = x + 1
+    println("ClassRDDManupulation_Ver4")
+    val incrementedListRDD = listRDD.map(increment(_))
+    incrementedListRDD.foreach(println)
+  }
+}
+object AllAboutSerialization extends App {
+
+  def simpleRDDManipulation(sc : SparkContext) = {
+
+    val listRDD = sc.parallelize(List(1,2,3,4,5,6,7,8,9,10), 2)
+    def increment(x: Int) = x + 1
+    println("simpleRDDManipulation")
+    val incrementedListRDD = listRDD.map(increment(_))
+    incrementedListRDD.foreach(println)
+  }
+
+  val sc = TejUtils.getSparkContext("AllAboutSerialization")
+
+  simpleRDDManipulation(sc)
+
+  val classRDDManupulation_Ver2 = new ClassRDDManupulation_Ver2
+  classRDDManupulation_Ver2.listIncrement(sc)
+
+  val classRDDManupulation_Ver3 = new ClassRDDManupulation_Ver3
+  classRDDManupulation_Ver3.listIncrement(sc)
+
+  ClassRDDManupulation_Ver4.listIncrement(sc)
+
+  val classRDDManupulation_Ver1 = new ClassRDDManupulation_Ver1
+  classRDDManupulation_Ver1.listIncrement(sc)
 }
