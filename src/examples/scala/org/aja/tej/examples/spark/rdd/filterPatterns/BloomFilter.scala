605b3e5fc9918093830e4ee9c2dd80a9581b0211
Mageswaran Dhandapani <mageswaran.dhanadapani@harman.com>
2016-03-11 12:13:08 +1100

    hive syntax

---
+++ src/examples/scala/org/aja/tej/examples/spark/rdd/filterPatterns/BloomFilter.scala
@@ -0,0 +1,24 @@
+package org.aja.tej.examples.spark.rdd.filterPatterns
+
+import breeze.util.BloomFilter
+import org.aja.tej.utils.TejUtils
+
+/**
+  * Created by mdhandapani on 11/3/16.
+  */
+object BloomFilterExample extends App {
+
+  val sc = TejUtils.getSparkContext(this.getClass.getSimpleName)
+
+  val nums = List(1 to 20: _*).map(_.toString)
+  val rdd = sc.parallelize(nums,5)
+
+  val bf = rdd.mapPartitions{ iter =>
+    val bf = BloomFilter.optimallySized[String](10000, 0.0001)
+    iter.foreach(i => bf += i)
+    Iterator(bf)
+  }.reduce(_ | _)
+
+  println(bf.contains("5"))
+  println(bf.contains("31"))
+}
